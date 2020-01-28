fd421d0e10c74a55d4559807354c5cbb3655c6ba
mireo <mireo91@gmail.com>
2020-01-28 21:39:48 +0100

    new

--- Resources/Private/Backend/RepeatableField/src/View/Repeatable.js
+++ Resources/Private/Backend/RepeatableField/src/View/Repeatable.js
@@ -24,6 +24,7 @@ export default class Repeatable extends PureComponent {
         isLoading: true
     };
 
+
     static propTypes = {
         identifier: PropTypes.string.isRequired,
         label: PropTypes.string.isRequired,
